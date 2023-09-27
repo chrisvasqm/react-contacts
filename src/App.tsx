a36a08a7b6cd17c6d9568191856f339245bc1e73
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-27 00:07:08 -0400

    Display peoples names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, List, ListItem } from '@chakra-ui/react';
+import data from './data/people.json';
 
 function App() {
-  return <Heading>Contacts</Heading>;
+  const people = data;
+
+  return (
+    <>
+      <Heading>Contacts</Heading>
+
+      <List>{people && people.map(person => <ListItem>{person.name}</ListItem>)}</List>
+    </>
+  );
 }
 
 export default App;
