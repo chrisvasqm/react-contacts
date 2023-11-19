b5e1006e9c5b952ca7cc15225e9b5bb7d18f1429
chrisvasqm <christianvasquezmelo@gmail.com>
2023-11-18 19:31:40 -0500

    Add card

--- src/pages/ContactDetailsPage.tsx
+++ src/pages/ContactDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { Heading } from '@chakra-ui/react';
+import { HStack, Image, VStack, Text, Card } from '@chakra-ui/react';
 import Contact from '../models/contact';
 import { useParams } from 'react-router-dom';
 
@@ -13,11 +13,21 @@ function ContactDetailsPage({ contacts }: Props) {
   const contact = contacts.find(c => c.id === parseInt(id, 10));
   if (!contact) return <div>Contact not found.</div>
 
-  return <div>
-    <Heading>Details</Heading>
-    <div>ID: {contact.id}</div>
-    <div>Name: {contact.name}</div>
-  </div>;
+  return <Card>
+    <HStack padding={2}>
+      <Image
+        width={'200px'}
+        aspectRatio={4 / 4}
+        objectFit={'cover'}
+        borderRadius={'full'}
+        src={contact.image}
+      />
+      <VStack align={'left'}>
+        <Text>ID: {contact.id}</Text>
+        <Text>Name: {contact.name}</Text>
+      </VStack>
+    </HStack>
+  </Card>
 }
 
 export default ContactDetailsPage;
